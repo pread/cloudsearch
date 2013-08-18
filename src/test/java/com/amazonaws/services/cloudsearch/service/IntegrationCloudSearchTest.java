5afd363d005dca7fd0f49ad73c89aa6b1789da33
Phillip Read <phillip.read@github>
2013-08-18 15:38:12 +0100

    Fix JUnit test cases

--- src/test/java/com/amazonaws/services/cloudsearch/service/IntegrationCloudSearchTest.java
+++ src/test/java/com/amazonaws/services/cloudsearch/service/IntegrationCloudSearchTest.java
@@ -1,33 +1,33 @@
 package com.amazonaws.services.cloudsearch.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import com.amazonaws.services.cloudsearch.AmazonCloudSearchClient;
 import com.amazonaws.services.cloudsearch.common.json.JsonUtils;
+import com.amazonaws.services.cloudsearch.model.DescribeDomainsResult;
+import com.amazonaws.services.cloudsearch.model.DomainStatus;
 import com.amazonaws.services.cloudsearch.model.sdf.Field;
 import com.amazonaws.services.cloudsearch.model.sdf.SearchDocumentAdd;
 import com.amazonaws.services.cloudsearch.model.sdf.SearchDocumentFormat;
 import com.amazonaws.services.cloudsearch.model.search.SearchResponse;
 import com.amazonaws.services.cloudsearch.model.upload.UploadResponse;
 import com.google.common.collect.ImmutableList;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.amazonaws.services.cloudsearch.AmazonCloudSearchClient;
-import com.amazonaws.services.cloudsearch.model.DescribeDomainsResult;
-import com.amazonaws.services.cloudsearch.model.DomainStatus;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.apache.log4j.Logger;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * A suite of tests for calling the TIA REST endpoint.
@@ -41,7 +41,8 @@ import static org.junit.Assert.assertTrue;
 		"classpath*:META-INF/spring/applicationContext*.xml" })
 public class IntegrationCloudSearchTest {
 
-    private static Logger LOG = Logger.getLogger(IntegrationCloudSearchTest.class);
+    /** The log. */
+    private static Log LOG = LogFactory.getLog(IntegrationCloudSearchTest.class);
 
     @Autowired(required=true)
     @Qualifier("cloudSearchClient")
@@ -51,7 +52,13 @@ public class IntegrationCloudSearchTest {
     @Qualifier("cloudSearchService")
     private CloudSearchService cloudSearchService;
 
+    private static ExpressionParser parser;
     private static final String DOMAIN_MOVIES = "imdb-movies";
+
+    @Before
+    public void initObjects() {
+        parser = new SpelExpressionParser();
+    }
 
 	/**
 	 * Find movies matching query.
@@ -60,23 +67,16 @@ public class IntegrationCloudSearchTest {
     public void cloudSearch() {
 
         DescribeDomainsResult domains = cloudSearch.describeDomains();
-        List<DomainStatus> list = domains.getDomainStatusList();
+        StandardEvaluationContext context = new StandardEvaluationContext(domains);
+        List<DomainStatus> matches = (List<DomainStatus>) parser.parseExpression("DomainStatusList.?[DomainName == '" + DOMAIN_MOVIES + "']").getValue(context);
 
-        DomainStatus domainStatus = null;
-        for(DomainStatus i : list) {
-            if (StringUtils.equals(DOMAIN_MOVIES, i.getDomainName())) {
-                domainStatus = i;
-                break;
-            }
-        }
-
-        if (null != domainStatus) {
+        if (!matches.isEmpty()) {
 
             String query = "Star Wars";
             String returnFields = "actor%2Cdirector%2Ctitle%2Cyear%2Ctext_relevance";
             String facet = "genre";
 
-            System.out.println("Number of searchable documents for " + domainStatus.getDomainName() + " is " + domainStatus.getNumSearchableDocs());
+            LOG.info("Number of searchable documents for " + matches.get(0).getDomainName() + " is " + matches.get(0).getNumSearchableDocs());
             for(int start = 0; start < 10000; start=start+10) {
 
                 SearchResponse searchResponse = cloudSearchService.cloudSearchRead(query, returnFields, start, facet);
@@ -98,19 +98,12 @@ public class IntegrationCloudSearchTest {
     public void cloudSearchActor() {
 
         DescribeDomainsResult domains = cloudSearch.describeDomains();
-        List<DomainStatus> list = domains.getDomainStatusList();
+        StandardEvaluationContext context = new StandardEvaluationContext(domains);
+        List<DomainStatus> matches = (List<DomainStatus>) parser.parseExpression("DomainStatusList.?[DomainName == '" + DOMAIN_MOVIES + "']").getValue(context);
 
-        DomainStatus domainStatus = null;
-        for(DomainStatus i : list) {
-            if (StringUtils.equals(DOMAIN_MOVIES, i.getDomainName())) {
-                domainStatus = i;
-                break;
-            }
-        }
+        if (!matches.isEmpty()) {
 
-        if (null != domainStatus) {
-
-            System.out.println("Number of searchable documents for " + domainStatus.getDomainName() + " is " + domainStatus.getNumSearchableDocs());
+            LOG.info("Number of searchable documents for " + matches.get(0).getDomainName() + " is " + matches.get(0).getNumSearchableDocs());
             int start = 0;
             String query = "(and actor:'Carrie Fisher' genre:'Horror')";
 
@@ -129,23 +122,16 @@ public class IntegrationCloudSearchTest {
     public void cloudSearchBooleanQuery() {
 
         DescribeDomainsResult domains = cloudSearch.describeDomains();
-        List<DomainStatus> list = domains.getDomainStatusList();
+        StandardEvaluationContext context = new StandardEvaluationContext(domains);
+        List<DomainStatus> matches = (List<DomainStatus>) parser.parseExpression("DomainStatusList.?[DomainName == '" + DOMAIN_MOVIES + "']").getValue(context);
 
-        DomainStatus domainStatus = null;
-        for(DomainStatus i : list) {
-            if (StringUtils.equals(DOMAIN_MOVIES, i.getDomainName())) {
-                domainStatus = i;
-                break;
-            }
-        }
-
-        if (null != domainStatus) {
+        if (!matches.isEmpty()) {
 
             String query = "(and director:'Lucas|Spielberg' (not actor:'\"Ford, Harrison\"'))";
             String returnFields = "actor%2Cdirector%2Ctitle%2Cyear%2Ctext_relevance";
             String facet = "genre";
 
-            System.out.println("Number of searchable documents for " + domainStatus.getDomainName() + " is " + domainStatus.getNumSearchableDocs());
+            LOG.info("Number of searchable documents for " + matches.get(0).getDomainName() + " is " + matches.get(0).getNumSearchableDocs());
             for(int start = 0; start < 10000; start=start+10) {
                 SearchResponse searchResponse = cloudSearchService.cloudSearchBooleanQuery(query, returnFields, start, facet);
                 int count = searchResponse.getFound().getCount();
@@ -164,57 +150,16 @@ public class IntegrationCloudSearchTest {
     public void cloudSearchPostSdf() {
 
         DescribeDomainsResult domains = cloudSearch.describeDomains();
-        List<DomainStatus> list = domains.getDomainStatusList();
+        StandardEvaluationContext context = new StandardEvaluationContext(domains);
+        List<DomainStatus> matches = (List<DomainStatus>) parser.parseExpression("DomainStatusList.?[DomainName == '" + DOMAIN_MOVIES + "']").getValue(context);
 
-        DomainStatus domainStatus = null;
-        for(DomainStatus i : list) {
-            if (StringUtils.equals(DOMAIN_MOVIES, i.getDomainName())) {
-                domainStatus = i;
-                break;
-            }
-        }
+        if (!matches.isEmpty()) {
 
-        if (null != domainStatus) {
+            SearchDocumentFormat item = testSdfDocument();
 
-            SearchDocumentFormat item = new SearchDocumentFormat();
-            List<SearchDocumentAdd> documents = new ArrayList<SearchDocumentAdd>();
-
-            Field fields = new Field();
-            fields.setTitle("The Seeker: The Dark Is Rising");
-            fields.setDirector("Cunningham, David L.");
-            fields.setYear(2007);
-            fields.setGenre(ImmutableList.of("Adventure","Drama","Fantasy","Thriller"));
-            fields.setActor(ImmutableList.of("McShane, Ian","Eccleston, Christopher","Conroy, Frances","Ludwig, Alexander","Crewson, Wendy","Warner, Amelia","Cosmo, James","Hickey, John Benjamin","Piddock, Jim","Lockhart, Emma"));
-
-            Field fields2 = new Field();
-            fields2.setTitle("Emma");
-            fields2.setDirector("McGrath, Douglas");
-            fields2.setYear(1996);
-            fields2.setGenre(ImmutableList.of("Comedy","Romance"));
-            fields2.setActor(ImmutableList.of("Paltrow, Gwyneth","Cumming, Alan","Collette, Toni","Northam, Jeremy","Scacchi, Greta","Cosmo, James","Thompson, Sophie","Law, Phyllida","Boardman, Lee","Byron, Kathleen","Hawthorne, Denys"));
-
-            SearchDocumentAdd z = new SearchDocumentAdd();
-            z.setId("tt0484562");
-            z.setVersion("2");
-            z.setLang("en");
-            z.setFields(fields);
-
-            SearchDocumentAdd z2 = new SearchDocumentAdd();
-            z2.setId("tt0116191");
-            z2.setVersion("2");
-            z2.setLang("en");
-            z2.setFields(fields2);
-
-            documents.add(z);
-            documents.add(z2);
-
-            item.setSearchDocumentAdds(documents);
-
-            LOG.debug("SDF Batch Upload Request : " + JsonUtils.marshal(item.getSearchDocumentAdds()));
-
+            LOG.info("SDF Batch Upload Request : " + JsonUtils.marshal(item.getSearchDocumentAdds()));
             UploadResponse result = cloudSearchService.batch(item);
-
-            LOG.debug("SDF Batch Upload Response : " + JsonUtils.marshal(result));
+            LOG.info("SDF Batch Upload Response : " + JsonUtils.marshal(result));
 
             assertNotNull(result);
             assertEquals("success", result.getStatus());
@@ -222,8 +167,44 @@ public class IntegrationCloudSearchTest {
             assertTrue(result.getDeletes() == 0);
             assertTrue(result.getErrors().size() == 0);
             assertTrue(result.getWarnings().size() == 0);
-
         }
     }
 
+    private SearchDocumentFormat testSdfDocument() {
+
+        SearchDocumentFormat item = new SearchDocumentFormat();
+        List<SearchDocumentAdd> documents = new ArrayList<SearchDocumentAdd>();
+
+        Field fields = new Field();
+        fields.setTitle("The Seeker: The Dark Is Rising");
+        fields.setDirector("Cunningham, David L.");
+        fields.setYear(2007);
+        fields.setGenre(ImmutableList.of("Adventure","Drama","Fantasy","Thriller"));
+        fields.setActor(ImmutableList.of("McShane, Ian","Eccleston, Christopher","Conroy, Frances","Ludwig, Alexander","Crewson, Wendy","Warner, Amelia","Cosmo, James","Hickey, John Benjamin","Piddock, Jim","Lockhart, Emma"));
+
+        Field fields2 = new Field();
+        fields2.setTitle("Emma");
+        fields2.setDirector("McGrath, Douglas");
+        fields2.setYear(1996);
+        fields2.setGenre(ImmutableList.of("Comedy","Romance"));
+        fields2.setActor(ImmutableList.of("Paltrow, Gwyneth","Cumming, Alan","Collette, Toni","Northam, Jeremy","Scacchi, Greta","Cosmo, James","Thompson, Sophie","Law, Phyllida","Boardman, Lee","Byron, Kathleen","Hawthorne, Denys"));
+
+        SearchDocumentAdd z = new SearchDocumentAdd();
+        z.setId("tt0484562");
+        z.setVersion("2");
+        z.setLang("en");
+        z.setFields(fields);
+
+        SearchDocumentAdd z2 = new SearchDocumentAdd();
+        z2.setId("tt0116191");
+        z2.setVersion("2");
+        z2.setLang("en");
+        z2.setFields(fields2);
+
+        documents.add(z);
+        documents.add(z2);
+
+        item.setSearchDocumentAdds(documents);
+        return item;
+    }
 }
